1934cb96de451e65b4313b4402d5f8c4564c4aa9
syaofox <syaofox@gmail.com>
2022-03-29 19:12:21 +0800

    update fonts

--- config.def.h
+++ config.def.h
@@ -5,8 +5,11 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11","Symbols Nerd Font:style=1000-em:size=13:antialias=true:autohint=true","WenQuanYi Micro Hei:style=Regular:size=11:antialias=true:autohint=true","Symbola:style=Regular:size=12:antialias=true:autohint=true"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[] 			= {
+	"BlexMono Nerd Font:size=12:antialias=true:autohint=true",
+    "Sarasa UI SC:size=12:antialias=true:autohint=true",
+};
+static const char dmenufont[] 		= "BlexMono Nerd Font:size=12:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
