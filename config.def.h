c3c7118828d29c8252ab38ef8be405bd5856963d
syaofox <syaofox@gmail.com>
2022-03-31 10:49:58 +0800

    font site

--- config.def.h
+++ config.def.h
@@ -6,10 +6,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[] 			= {
-	"BlexMono Nerd Font:size=12:antialias=true:autohint=true",
-    "Sarasa UI SC:size=12:antialias=true:autohint=true",
+	"BlexMono Nerd Font:size=10:antialias=true:autohint=true",
+    "Sarasa UI SC:size=10:antialias=true:autohint=true",
 };
-static const char dmenufont[] 		= "BlexMono Nerd Font:size=12:antialias=true:autohint=true";
+static const char dmenufont[] 		= "BlexMono Nerd Font:size=10:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
