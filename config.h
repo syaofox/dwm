603e4df286fa58bc3b60ac19ccef7d2dd9b29566
syaofox <syaofox@gmail.com>
2025-10-21 17:12:24 +0800

    修改配置

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const unsigned int gappih    = 4;       /* horiz inner gap between window
 static const unsigned int gappiv    = 2;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 2;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 2;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:style=Regular:size=10" };
-static const char dmenufont[]       = "JetBrains Mono:style=Regular:size=10";
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "JetBrains Mono:style=Regular:size=12" };
+static const char dmenufont[]       = "JetBrains Mono:style=Regular:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
