7c5d7b3aab7b1aa8e729947c43d0bb19e963a6cc
syaofox <syaofox@gmail.com>
2025-10-21 16:56:04 +0800

    彩色状态条+补丁

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int gappoh    = 2;       /* horiz outer gap between window
 static const unsigned int gappov    = 2;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono:style=Regular:size=10" };
 static const char dmenufont[]       = "JetBrains Mono:style=Regular:size=10";
 static const char col_gray1[]       = "#222222";
