4541289247764f11491ed916aa9495a8f8e50b5e
syaofox <syaofox@gmail.com>
2026-04-08 20:53:37 +0800

    feat: 新增窗口标题区配色方案 SchemeTitle
    变更内容:
    - config.def.h: 添加 SchemeTitle 颜色定义，用于窗口标题区域
    - dwm.c: 在 enum 中添加 SchemeTitle 枚举值
    - dwm.c: drawbar 中使用 SchemeTitle 替代原来的 SchemeSel/SchemeNorm 渲染窗口标题区

--- config.def.h
+++ config.def.h
@@ -31,7 +31,8 @@ static const char *colors[][3]      = {
 	[SchemeSel]    = { col_gray4, col_cyan,  col_cyan  }, /* 选中，无客户端 */
 	[SchemeOcc]    = { col_white, col_gray1, col_gray2 }, /* 未选中，有客户端 */
 	[SchemeSelOcc] = { col_white, col_cyan,  col_cyan  }, /* 选中，有客户端 */
-	[SchemeStatus] = { col_white, col_gray1, col_gray1 }, /* 状态栏 */
+	[SchemeStatus] = { col_gray3, col_gray1, col_gray1 }, /* 状态栏 */
+	[SchemeTitle]  = { col_gray4, col_gray1, col_gray2 }, /* 窗口标题区 */
 };
 
 /* tagging */
