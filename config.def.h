05f79d2bef5a71f08fc131f1962171d77c293bf0
syaofox <syaofox@gmail.com>
2026-04-08 15:30:54 +0800

    feat: 添加 viewontag 功能，切换标签时自动切换视图
    变更内容:
    - config.def.h: 添加 viewontag 配置项，默认为 True
    - dwm.c: 在 tag() 函数中实现切换标签时自动切换视图的逻辑

--- config.def.h
+++ config.def.h
@@ -10,6 +10,7 @@ static const unsigned int gappov    = 4;        /* vertical outer gap */
 static const int smartgaps          = 0;        /* 1 means no outer gap when 1 client */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font Propo:style=Bold:size=10" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font Propo:style=Bold:size=10";
 static const char col_gray1[]       = "#222222";
