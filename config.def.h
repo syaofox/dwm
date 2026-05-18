5c3b6f572adc8b896c0c895fb5e7c915daa2b81e
syaofox <syaofox@gmail.com>
2026-05-18 20:29:35 +0800

    快捷键绑定

--- config.def.h
+++ config.def.h
@@ -232,7 +232,7 @@ static const Key keys[] = {
 	// { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	// { MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY,                       XK_n,  cyclelayout,    {.i = +1} },
-	// { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglealwaysontop, {0} },
 	{ MODKEY,			            XK_z,      togglefullscreen, {0} },
 	// { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
